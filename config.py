7f0723363ae421bf038da8c25c77a5ff1bb7efde
King.John <zhouyq520@hotmail.com>
2024-10-21 09:29:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings
 from typing import Optional
 
 class Settings(BaseSettings):
-    TUSHARE_TOKEN: str = "1ba1cfe5767fc1462c5bd00cbd911016ec18cf65233894e65fd0c4df"
+    TUSHARE_TOKEN: str = ""
     OPENAI_API_KEY: str
-    API_URL: str = "https://oneapi.intellectapi.com/v1/chat/completions"
+    API_URL: str = ""
     MODEL_NAME: str = "groq"
     BASE_URL: str = "http://127.0.0.1:8000"
     FONT_PATH: str = "./static/fonts/imhei.ttf"
